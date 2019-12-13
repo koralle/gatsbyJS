8f8940ad77d0fdefa40311e0908ba83d3e95935d
Mamoru Kosugiyama <mkhd7930@gmail.com>
2019-12-13 22:16:55 +0900

    create header.js and contact.js

--- tutorial/hello-world/src/pages/index.js
+++ tutorial/hello-world/src/pages/index.js
@@ -1,8 +1,14 @@
 import React from "react"
+import { Link } from "gatsby";
+import Header from './header';
 
 const App = () => {
     return (
-        <h1>Hello, world!</h1>
+        <div className="app-root">
+            <Header headerText ="Hello, Gatsby!" />
+            <Link to="/contact/">Contact</Link>
+            <h1>Hello, world!</h1>
+        </div>
     );
 }
 
